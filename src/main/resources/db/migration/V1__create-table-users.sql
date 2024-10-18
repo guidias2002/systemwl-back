525c81dbc9c8d5636a46b96bcdb97ada8420e759
Guilherme Dias <guidias200220@gmail.com>
2024-10-18 15:30:31 -0300

    faet: create v1__create-table-users

---
+++ src/main/resources/db/migration/V1__create-table-users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE gcd_users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    login VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
